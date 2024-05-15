b78956ad6f76c378150c89ea379204e8c7bbe9c2
pratham_srnk <pratham@appointy.com>
2024-05-15 10:17:40 +0530

    Changed channel from WS to HTTP

--- app/search/models/channel_search/channel_search_connection.py
+++ app/search/models/channel_search/channel_search_connection.py
@@ -1,8 +1,8 @@
 from fastapi import WebSocket
-from youtubesearchpython import Playlist
+from youtubesearchpython import Channel
 
 
 class ChannelSearchConnection:
-    def __init__(self, websocket: WebSocket, search_obj: Playlist | None):
+    def __init__(self, websocket: WebSocket, search_obj: Channel | None):
         self.websocket = websocket
         self.search_obj = search_obj
--- app/search/services/channel_search_response.py
+++ app/search/services/channel_search_response.py
@@ -1,42 +1,23 @@
-import uuid
-from fastapi import WebSocket
-from ..models.channel_search.channel_search_connection import ChannelSearchConnection
-from youtubesearchpython import Playlist, playlist_from_channel_id
+from youtubesearchpython import Channel, ChannelRequestType
 
 
 class ChannelSearchService:
-    def __init__(self, ):
-        self.connected_users: dict = {}
-
-    def accept(self, websocket: WebSocket):
-        user_id = str(uuid.uuid4())
-        self.connected_users[user_id] = ChannelSearchConnection(websocket, None)
+    def __init__(self, id):
+        self.id = id
         pass
 
-    async def init_search(self, data, websocket: WebSocket):
-        try:
-            # Fetch user_id from connected_users
-            user_id = next(key for key, value in self.connected_users.items() if value.websocket == websocket)
+    def get_info(self):
+        info_search: Channel = Channel(channel_id=self.id, request_type=ChannelRequestType.info)
+        return info_search.result
 
-            # Validate Search Preferences
-            search_obj: Playlist = Playlist(playlist_from_channel_id(data['id']))
-            self.connected_users[user_id].search_obj = search_obj
-            await websocket.send_json({"success": True, "data": search_obj.videos})
-        except ValueError as e:
-            await websocket.send_json({"success": False, "message": str(e)})
-
-    async def next_search(self, websocket: WebSocket):
-        user_id = next(key for key, value in self.connected_users.items() if value.websocket == websocket)
-        search_obj: Playlist | None = self.connected_users[user_id].search_obj
-        if search_obj is None:
-            await websocket.send_json({"success": False, "message": "No search object found"})
-            return
-        else:
-            search_obj.getNextVideos()
-            await websocket.send_json({"success": True, "data": search_obj.videos})
-
-    def on_disconnect(self, websocket: WebSocket):
-        user_id = next(key for key, value in self.connected_users.items() if value.websocket == websocket)
-        if user_id in self.connected_users:  # Check if user ID exists before deletion
-            del self.connected_users[user_id]
-        print(f"Connection closed by client: {user_id}")
+    def get_playlists(self):
+        playlist_search: Channel = Channel(channel_id=self.id, request_type=ChannelRequestType.playlists)
+        playlists = playlist_search.result['playlists']
+        for playlist in playlists:
+            playlist['channel'] = {
+                'name': playlist_search.result['title'],
+                'id': playlist_search.result['id'],
+                'link': playlist_search.result['url'],
+                'thumbnails': playlist_search.result['thumbnails']
+            }
+        return playlists
